7f51312ee66b80129cd7ab2c0244df20d9902689
william-rauwens-oiver <william.rauwens-oliver@laplateforme.io>
2024-03-25 10:46:59 +0100

    code job05 finish

---
+++ Jour01/job05/script.js
@@ -0,0 +1,8 @@
+function AfficherJourSemaine () {
+    let jours = ['Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi', 'Dimanche'];
+    for (var i = 0; i < jours.length; i++) {
+        console.log(jours[i])
+    }
+}
+
+AfficherJourSemaine();
