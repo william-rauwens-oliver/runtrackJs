76a522522daea5df382018c6e6c059c86437aba5
william-rauwens-oiver <william.rauwens-oliver@laplateforme.io>
2024-03-26 09:43:08 +0100

    job2 finish

---
+++ Jour02/job02/script.js
@@ -0,0 +1,10 @@
+function showhide() {
+    var article = document.getElementById("article");
+    if (article.style.display === "none") {
+      article.style.display = "block";
+    } else {
+      article.style.display = "none";
+    }
+  }
+  
+  document.getElementById("button").addEventListener("click", showhide); 
