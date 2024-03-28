8eed49abeac882bb28a89741d7533bfb16e29981
william-rauwens-oiver <william.rauwens-oliver@laplateforme.io>
2024-03-28 09:44:21 +0100

    add code for job01

---
+++ Jour04/job01/script.js
@@ -0,0 +1,15 @@
+document.getElementById("button").addEventListener("click", function() {
+    fetch("expression.txt")
+    .then(response => {
+        if (!response.ok) {
+            throw new Error("Erreur de récupération du fichier.");
+        }
+        return response.text();
+    })
+    .then(data => {
+        document.getElementById("expression").innerText = data;
+    })
+    .catch(error => {
+        console.error("Erreur:", error);
+    });
+});
